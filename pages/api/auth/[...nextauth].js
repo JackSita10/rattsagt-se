39f92675a6e4e38ace41933912dcffd1c7c1284b
JackSita10 <johanna.hallqvist@hotmail.com>
2025-06-11 13:57:46 +0200

    Add files via upload
    dash

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize(credentials) {
+        if (credentials.username === "admin" && credentials.password === "admin") {
+          return { id: 1, name: "Admin" };
+        }
+        return null;
+      }
+    })
+  ],
+  pages: {
+    signIn: '/login'
+  }
+});
